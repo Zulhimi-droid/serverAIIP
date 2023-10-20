2ea6e754799cb15c0854d59142fb6af5db87cde1
Zulhimi-droid <122745094+Zulhimi-droid@users.noreply.github.com>
2023-10-20 14:46:32 +0800

    latest (ZUL)

--- component/register/register.js
+++ component/register/register.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const pool = require('../db/db'); // Import your pool configuration from db.js
+const pool = require('../db/db');
 const bcrypt = require('bcrypt');
+const { v4: uuidv4 } = require('uuid'); // Import the UUID function
 
 const registrationRouter = express.Router();
 
@@ -8,29 +9,40 @@ registrationRouter.get('/', (req, res) => {
   res.render('register');
 });
 
-// Handle the POST request for user registration
 registrationRouter.post('/form', async (req, res) => {
   const { first_name, last_name, age, address, email, password } = req.body;
 
   // Hash the password securely
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  // Get the formatted timestamp
-  const formattedTimestamp = new Date().toISOString().slice(0, 16).replace("T", " ");
+  // Check if the email already exists in the database
+  const checkEmailQuery = 'SELECT email FROM user_details WHERE email = $1';
+  const emailExists = await pool.query(checkEmailQuery, [email]);
 
-  try {
-    const query = `
-      INSERT INTO user_details (first_name, last_name, age, address, email, password, first_register)
-      VALUES ($1, $2, $3, $4, $5, $6, $7)
-    `;
-    const values = [first_name, last_name, age, address, email, hashedPassword, formattedTimestamp];
+  if (emailExists.rows.length > 0) {
+    // Email already exists, show the error message
+    res.redirect('/register?error=email-in-use');
+  } else {
+    // Generate a UUID for the user
+    const userId = uuidv4();
 
-    await pool.query(query, values);
+    // Get the formatted timestamp
+    const formattedTimestamp = new Date().toISOString().slice(0, 16).replace('T', ' ');
 
-    res.redirect('/register?success=true');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal server error');
+    try {
+      const insertQuery = `
+        INSERT INTO user_details (id, first_name, last_name, age, address, email, password, first_register)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+      `;
+      const values = [userId, first_name, last_name, age, address, email, hashedPassword, formattedTimestamp];
+
+      await pool.query(insertQuery, values);
+
+      res.redirect('/register?success=true');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal server error');
+    }
   }
 });
 
